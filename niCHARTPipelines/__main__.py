7b939fb0bb1fd099e1b04926dead5e352bbd7f2c
Ashish Singh <ashish.singh@uphs.upenn.edu>
2023-01-26 06:34:37 -0500

    Add more params and pass them all to pipeline

--- niCHARTPipelines/__main__.py
+++ niCHARTPipelines/__main__.py
@@ -13,9 +13,11 @@ def main():
     parser = argparse.ArgumentParser(description='niCHART Data Preprocessing Pipelines')
     parser.add_argument('--inImg', type=str, help='Input T1 image file path.', default=None, required=True)
     parser.add_argument('--DLICVmdl', type=str, help='DLICV model path.', default=None, required=True)
-    parser.add_argument('--DLMUSEmdl', type=str, help='DLMUSE Model path.', default=None, required=False)
+    parser.add_argument('--DLMUSEmdl', type=str, help='DLMUSE Model path.', default=None, required=True)
     parser.add_argument('--pipelineType', type=str, help='Specify type of pipeline[structural, dti, fmri].', default=None, required=True)
-    parser.add_argument('--outImg', type=str, help='Output file name with extension.', default=None, required=False)
+    parser.add_argument('--outFile', type=str, help='Output file name with extension.', default=None, required=True)
+    parser.add_argument('--scanID', type=str, help='scan id.', default=None, required=True)
+    parser.add_argument('--derivedROIMappingsFile', type=str, help='derived MUSE ROI mappings file.', default=None, required=True)
     
     args = parser.parse_args(sys.argv[1:])
 
@@ -23,10 +25,12 @@ def main():
     DLICVmdl = args.DLICVmdl
     DLMUSEmdl = args.DLMUSEmdl
     pipelineType = args.pipelineType
-    outImg = args.outImg
+    outFile = args.outFile
+    scanID = args.scanID
+    roiMappingsFile = args.derivedROIMappingsFile
 
     if(pipelineType == "structural"):
-        Structural.run_structural_pipeline(inImg,DLICVmdl,DLMUSEmdl,outImg)
+        Structural.run_structural_pipeline(inImg,DLICVmdl,DLMUSEmdl,outFile,scanID,roiMappingsFile)
     elif(pipelineType == "fmri"):
         print("Coming soon.")
         exit()
