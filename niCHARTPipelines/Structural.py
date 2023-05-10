5b4c6d74add06059a34f80cea623f940ef53efce
Ashish Singh <ashish.singh@uphs.upenn.edu>
2023-05-09 22:59:16 -0400

    Modified structural pipeline

--- niCHARTPipelines/Structural.py
+++ niCHARTPipelines/Structural.py
@@ -31,15 +31,6 @@ def run_structural_pipeline(inImg,DLICVmdl,DLMUSEmdl,outFile, MuseMappingFile):
     #dlicv.run()
     print('outdir: ', dlicv.inputs.out_dir)
     print("DLICV done")
-    # Create DLICV Node
-    # dlicv = Node(DeepMRSegInterface.DeepMRSegInference(), name='dlicv')
-    # dlicv.inputs.in_file = Path(inImg)
-    # lps_mdl_path = os.path.join(DLICVmdl,'LPS')
-    # if(os.path.isdir(lps_mdl_path)):
-    #     dlicv.inputs.mdl_dir1 = lps_mdl_path
-    # dlicv.inputs.out_file = os.path.join(outDir,'dlicv.nii.gz')
-    # dlicv.inputs.batch_size = 4
-    # dlicv.inputs.nJobs = 1
 
     # Create Apply Mask Node
     maskImage = Node(MaskImageInterface.MaskImage(), name='maskImage')
@@ -66,23 +57,7 @@ def run_structural_pipeline(inImg,DLICVmdl,DLMUSEmdl,outFile, MuseMappingFile):
     os.mkdir(muse.inputs.out_dir)
     print('outdir: ', muse.inputs.out_dir)
     print("MUSE done")
-    
-    
-    # Create MUSE Node
-    # muse = Node(DeepMRSegInterface.DeepMRSegInference(), name='muse')
-    # muse_lps_mdl_path = os.path.join(DLMUSEmdl,'LPS')
-    # #muse_psl_mdl_path = os.path.join(DLMUSEmdl,'PSL')      ## We run all in one orientation in tests
-    # #muse_slp_mdl_path = os.path.join(DLMUSEmdl,'SLP')
-    # if(os.path.isdir(muse_lps_mdl_path)):
-    #     muse.inputs.mdl_dir1 = muse_lps_mdl_path
-    # #if(os.path.isdir(muse_psl_mdl_path)):
-    #     #muse.inputs.mdl_dir2 = muse_psl_mdl_path
-    # #if(os.path.isdir(muse_slp_mdl_path)):
-    #     #muse.inputs.mdl_dir3 = muse_slp_mdl_path        
-    # muse.inputs.out_file = os.path.join(outDir,'muse.nii.gz')
-    # muse.inputs.batch_size = 4
-    # muse.inputs.nJobs = 1
-    
+     
     #create muse relabel Node
     relabel = Node(ROIRelabelInterface.ROIRelabel(), name='relabel')
     relabel.inputs.in_dir = Path(inDir)
@@ -96,10 +71,10 @@ def run_structural_pipeline(inImg,DLICVmdl,DLMUSEmdl,outFile, MuseMappingFile):
     print("relabeling done")
 
     # Create roi csv creation Node
-    roi_csv = Node(CalculateROIVolumeInterface.CalculateROIVolume(), name='roi-volume-csv')
-    roi_csv.inputs.map_csv_file = Path(roiMappingsFile)
-    roi_csv.inputs.scan_id = str(scanID)
-    roi_csv.inputs.out_file = Path(outFile)
+    #roi_csv = Node(CalculateROIVolumeInterface.CalculateROIVolume(), name='roi-volume-csv')
+    #roi_csv.inputs.map_csv_file = Path(roiMappingsFile)
+    #roi_csv.inputs.scan_id = str(scanID)
+    #roi_csv.inputs.out_file = Path(outFile)
 
     #create working dir in output dir for now
     basedir = os.path.join(outDir,'working_dir')
