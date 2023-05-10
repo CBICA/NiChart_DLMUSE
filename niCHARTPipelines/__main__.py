15581a31015bcce771c91374dbcf18c153008299
Ashish Singh <ashish.singh@uphs.upenn.edu>
2023-05-09 21:53:40 -0400

    Modifying main

--- niCHARTPipelines/__main__.py
+++ niCHARTPipelines/__main__.py
@@ -13,21 +13,23 @@ def main():
     parser = argparse.ArgumentParser(description='niCHART Data Preprocessing Pipelines')
     parser.add_argument('--inImg', type=str, help='Input T1 image file path.', default=None, required=True)
     parser.add_argument('--DLICVmdl', type=str, help='DLICV model path.', default=None, required=True)
-    #parser.add_argument('--DLMUSEmdl', type=str, help='DLMUSE Model path.', default=None, required=True)
+    parser.add_argument('--DLMUSEmdl', type=str, help='DLMUSE Model path.', default=None, required=True)
     #parser.add_argument('--pipelineType', type=str, help='Specify type of pipeline[structural, dti, fmri].', default=None, required=True)
     parser.add_argument('--outFile', type=str, help='Output file name with extension.', default=None, required=True)
     #parser.add_argument('--scanID', type=str, help='scan id.', default=None, required=True)
     #parser.add_argument('--derivedROIMappingsFile', type=str, help='derived MUSE ROI mappings file.', default=None, required=True)
+    parser.add_argument('--MuseROIMappingsFile', type=str, help='MUSE ROI mappings file.', default=None, required=True)
     
     args = parser.parse_args(sys.argv[1:])
 
     inImg = args.inImg
     DLICVmdl = args.DLICVmdl
-    #DLMUSEmdl = args.DLMUSEmdl
+    DLMUSEmdl = args.DLMUSEmdl
     #pipelineType = args.pipelineType
     outFile = args.outFile
     #scanID = args.scanID
     #roiMappingsFile = args.derivedROIMappingsFile
+    MuseroiMappingsFile = args.MuseROIMappingsFile
 
     # if(pipelineType == "structural"):
     #     Structural.run_structural_pipeline(inImg,DLICVmdl,DLMUSEmdl,outFile,scanID,roiMappingsFile)
@@ -40,7 +42,7 @@ def main():
     # else:
     #     print("Only [structural, dti and fmri] pipelines are supported.")
     #     exit()
-    Structural.run_structural_pipeline(inImg,DLICVmdl,outFile)
+    Structural.run_structural_pipeline(inImg,DLICVmdl,DLMUSEmdl,outFile,MuseroiMappingsFile)
 
 
 
