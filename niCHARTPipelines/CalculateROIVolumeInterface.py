37edf14b3d000c7a443aeaa688a13f2429a76a6c
Ashish Singh <ashish.singh@uphs.upenn.edu>
2023-05-09 23:39:14 -0400

    Modified vol interface

--- niCHARTPipelines/CalculateROIVolumeInterface.py
+++ niCHARTPipelines/CalculateROIVolumeInterface.py
@@ -1,16 +1,25 @@
-from nipype.interfaces.base import BaseInterfaceInputSpec, BaseInterface, File, TraitedSpec, traits
-from . import ROIVolumeCalculator as volCalculator
+from nipype.interfaces.base import BaseInterfaceInputSpec, BaseInterface, File, Directory, TraitedSpec, traits
+import ROIVolumeCalculator as volCalculator
+from pathlib import Path
+import os
+
+###---------utils----------------
+def get_file_basename_without_extension(filepath):
+    return os.path.basename(filepath).split('.', 1)[0]
 
 ###---------Interface------------
 
 class CalculateROIVolumeInputSpec(BaseInterfaceInputSpec):
-    mask_file = File(exists=True, mandatory=True, desc='the input mask image')
+    #mask_file = File(exists=True, mandatory=True, desc='the input mask image')
     map_csv_file = File(exists=True, mandatory=True, desc='the map csv file')
     scan_id = traits.Str(mandatory=True,desc='scan ID')
-    out_file = File(mandatory=True, desc='the output csv file') 
+    #out_file = File(mandatory=True, desc='the output csv file')
+    in_dir = Directory(mandatory=True, desc='the input dir')
+    out_dir = Directory(mandatory=True, desc='the output dir') 
 
 class CalculateROIVolumeOutputSpec(TraitedSpec):
-    out_file = File(desc='the output csv file')
+    #out_file = File(desc='the output csv file')
+    out_dir = File(desc='the output image')
 
 class CalculateROIVolume(BaseInterface):
     input_spec = CalculateROIVolumeInputSpec
@@ -19,14 +28,19 @@ class CalculateROIVolume(BaseInterface):
     def _run_interface(self, runtime):
 
         # Call our python code here:
-        volCalculator.calculate_volume(
-            self.inputs.mask_file,
-            self.inputs.map_csv_file,
-            self.inputs.scan_id,
-            self.inputs.out_file
-        )
+        infiles = Path(self.inputs.in_dir).glob('*.nii.gz')
+        for in_file in infiles:
+          basename_without_ext = get_file_basename_without_extension(in_file)
+          out_file = os.path.join(self.inputs.out_dir,basename_without_ext) + '.csv'
+          
+          volCalculator.calculate_volume(
+              in_file,
+              self.inputs.map_csv_file,
+              self.inputs.scan_id,
+              out_file
+          )
         # And we are done
         return runtime
 
     def _list_outputs(self):
-        return {'out_file': self.inputs.out_file}
+        return {'out_dir': self.inputs.out_dir}
