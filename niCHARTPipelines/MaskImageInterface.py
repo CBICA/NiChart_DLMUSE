3fcc95487d9908ad505f190cf29e129326c45745
Ashish Singh <ashish.singh@pennmedicine.upenn.edu>
2023-05-15 11:25:28 -0400

    Cleanup mask image interface script

--- niCHARTPipelines/MaskImageInterface.py
+++ niCHARTPipelines/MaskImageInterface.py
@@ -14,11 +14,8 @@ def get_split_filename(basename):
 ###---------Interface------------
 
 class MaskImageInputSpec(BaseInterfaceInputSpec):
-    #in_file = File(exists=True, mandatory=True, desc='the input image')
     in_dir = Directory(mandatory=True, desc='the input dir')
-    #out_file = File(mandatory=True, desc='the output image')
     out_dir = Directory(mandatory=True, desc='the output dir') 
-    #mask_file = File(desc='the input mask')
     mask_dir =Directory(desc='the input mask directory')
 
 class MaskImageOutputSpec(TraitedSpec):
@@ -35,26 +32,16 @@ class MaskImage(BaseInterface):
         infiles = Path(self.inputs.in_dir).glob('*.nii.gz')
         maskfiles = Path(self.inputs.mask_dir).glob('*.nii.gz')
 
-        # if(len(infiles) != len(maskfiles)):
-        #     print('something is not right! Your input files and DLICV output file count does not match, Exiting.' )
-        #     exit()
-        print(maskfiles)
-        print(infiles)
         for in_file in infiles:
           basename_without_ext = get_file_basename_without_extension(in_file)
-          print('basename_without_ext: ', basename_without_ext)
           in_file_split_name_ = get_split_filename(basename_without_ext)
-          print('needed_name: ', in_file_split_name_)
-          print('in_file: ', in_file)
 
           for mask_file in maskfiles:
-            print('mask_file: ', mask_file)
             mask_file_name_without_ext = get_file_basename_without_extension(mask_file)
 
             ##TODO compare basenames only. make sure that DLICV output names match input names first
             if(in_file_split_name_ == mask_file_name_without_ext):
                 out_file = os.path.join(self.inputs.out_dir,basename_without_ext) + '.nii.gz'
-                print('out-file: ',out_file)
                 masker.apply_mask(
                     in_file,
                     mask_file,
