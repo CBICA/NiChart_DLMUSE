24c8787adafa34a9a906a388aec6f5147b76a607
Ashish Singh <ashish.singh@pennmedicine.upenn.edu>
2023-05-15 11:26:35 -0400

    Clean up nnunet interface script

--- niCHARTPipelines/nnUNetInterface.py
+++ niCHARTPipelines/nnUNetInterface.py
@@ -4,11 +4,9 @@ import os
 ###--------Interface--------
 class nnUNetInferenceInputSpec(CommandLineInputSpec):
     in_dir = Directory(mandatory=True,argstr='-i %s', position=0,desc='the input folder')
-    #mdl_dir = Directory( argstr='%s',desc='the input model')
     f_val = traits.Int( argstr='-f %d', desc="f val: default 0")
     t_val = traits.Int(argstr='-t %d', desc="t val: default 803")
     m_val = traits.Str(argstr='-m %s',desc="m val: default 3d_fullres")
-    #gpu_val = traits.Str(argstr='--all_in_gpu %s',desc="gpu val: default True")
     out_dir = Directory(mandatory=True,argstr='-o %s', position=-1, desc='the output folder')
 
 class nnUNetInferenceOutputSpec(TraitedSpec):
@@ -24,16 +22,3 @@ class nnUNetInference(CommandLine):
             outputs['out_dir'] = self.inputs.out_dir
             return outputs
 
-#muse = nnUNetInference()
-#muse.inputs.in_dir = '/niCHARTPipelines/sharedFolder/Data/'
-##model_loc = '/niCHARTPipelines/sharedFolder/Model/'
-##os.environ['RESULTS_FOLDER'] = model_loc
-##mdl_dir = '/niCHARTPipelines/sharedFolder/DLMUSE_Model/'
-#mdl_dir = '/niCHARTPipelines/sharedFolder/Model'
-#os.environ['RESULTS_FOLDER'] = mdl_dir
-#muse.inputs.f_val = 0
-#muse.inputs.t_val = 803
-#muse.inputs.m_val = "3d_fullres"
-##muse.gpu_val = True
-#muse.inputs.out_dir = '/niCHARTPipelines/sharedFolder/Results2'
-#r = muse.run()
