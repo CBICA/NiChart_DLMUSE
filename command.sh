df920376d198c09c50b1d0865fe98177184071c4
George Aidinis <gaidinis@live.com>
2023-06-21 15:17:44 -0400

    added example command

---
+++ command.sh
@@ -0,0 +1,11 @@
+docker run --rm -v $(pwd)/temp/nnUNet_raw_data_base/nnUNet_raw_data:/input \
+                -v$(pwd)/temp/nnUNet_out:/output:rw \
+                -v$(pwd)/temp/:/mappings \
+                -v$(pwd)/temp/nnUNet_model:/models \
+                -it aidinisg/nichartpipelines:0.1.3 -i /input/ \
+                                                -o /output \
+                                                -p structural \
+                                                -s 123 \
+                                                --derived_ROI_mappings_file /mappings/MUSE_mapping_derived_rois.csv \
+                                                --MUSE_ROI_mappings_file /mappings/MUSE_mapping_consecutive_indices.csv \
+                                                --results_folder /models
