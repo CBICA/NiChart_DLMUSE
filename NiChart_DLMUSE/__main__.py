c63f85fe43abe35ab249a1d14f3281c0ebdfcac7
spirosmaggioros <spirosmag@ieee.org>
2025-01-10 22:26:59 +0000

    Reformat code

--- NiChart_DLMUSE/__main__.py
+++ NiChart_DLMUSE/__main__.py
@@ -7,10 +7,11 @@ Use of this source code is governed by license located in license file: https://
 
 import argparse
 
+import pkg_resources  # type: ignore
+
 from NiChart_DLMUSE.dlmuse_pipeline import run_ndlmuse_pipeline
 
-# VERSION = pkg_resources.require("NiChart_DLMUSE")[0].version
-VERSION = "1.0.9"
+VERSION = pkg_resources.require("NiChart_DLMUSE")[0].version
 
 
 def main() -> None:
