e41da5edc9f6ef00f1a6b80d91da7c5b5bbde98f
Tien Tong <tien.tong98@gmail.com>
2025-09-03 14:23:42 -0400

    add sitk.ConnectedComponent for refaced data

--- scripts/wrapper.py
+++ scripts/wrapper.py
@@ -1,16 +1,24 @@
 import argparse
-import shutil
 import os
+import shutil
 import tempfile
 from pathlib import Path
 
 # This wrapper script just adapts NiChart_DLMUSE to take two separate output args. Everything else is passed transparently
 
+
 def main():
     parser = argparse.ArgumentParser(description="Wrapper", allow_abbrev=False)
     parser.add_argument("-i", "--in_dir", required=True, help="Input directory")
-    parser.add_argument("-o1", "--out_segs", required=True, help="Output directory for segmentation files")
-    parser.add_argument("-o2", "--out_csvs", required=True, help="Output directory for CSV files")
+    parser.add_argument(
+        "-o1",
+        "--out_segs",
+        required=True,
+        help="Output directory for segmentation files",
+    )
+    parser.add_argument(
+        "-o2", "--out_csvs", required=True, help="Output directory for CSV files"
+    )
 
     # Parse known args; leave the rest for original app
     args, extra_args = parser.parse_known_args()
@@ -26,8 +34,14 @@ def main():
         tmp_output_path = Path(tmp_output)
 
         # Build command to run original application
-        cmd = ["NiChart_DLMUSE", "-i", input_dir, "-o", str(tmp_output_path)] + extra_args
-        command = ' '.join(cmd)
+        cmd = [
+            "NiChart_DLMUSE",
+            "-i",
+            input_dir,
+            "-o",
+            str(tmp_output_path),
+        ] + extra_args
+        command = " ".join(cmd)
         os.system(command)
 
         # Copy output files
@@ -39,6 +53,7 @@ def main():
                     dest_path = seg_dir / item.relative_to(tmp_output_path)
                     dest_path.parent.mkdir(parents=True, exist_ok=True)
                     shutil.copy2(item, dest_path)
+
 
 if __name__ == "__main__":
     main()
